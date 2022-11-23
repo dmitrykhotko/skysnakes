2ba70f87ed797ea2ddba957a9abc4ee78e1b5698
dmitrykhotko <dmitry.khotko@gmail.com>
2022-11-23 12:07:03 +0300

    small fixes

--- src/server/audio/index.ts
+++
@@ -1,2 +0,0 @@
-export { Audio } from './audio';
-export { SoundLib } from './soundLibrary';
--- src/server/audio/soundLibrary.ts
+++
@@ -1,5 +0,0 @@
-import MAIN_THEME from '../../assets/sounds/sw-main.mp3';
-
-export const SoundLib = {
-	mainTheme: MAIN_THEME
-};
